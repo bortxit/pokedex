adda0248d5abb40fd942d4c721a6b62d088f4c52
bortxit <bortxit@gmail.com>
2023-05-13 11:04:56 +0200

    Variables CSS e Inicio HTML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./reset_style.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>My Pokedex</nav>
       </header>
+      <section>
+        <div class="card-container">
+          <div class="card-title"></div>
+          <img class="image" src="" />
+          <div class="card-content"></div>
+        </div>
+      </section>
     </div>
   );
 }
